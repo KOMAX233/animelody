f15ae2bc39717ded25a87de5f3494f3f7e4c7530
KOMAX233 <fajasmine@outlook.com>
2024-12-10 13:34:08 -0500

    try catching all undefined routes

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,19 @@
 from app import create_app
+from flask import Flask, send_from_directory
+import os
+
+def create_app():
+    app = Flask(__name__, static_folder='build/static', template_folder='build')
+    
+    @app.route('/')
+    def index():
+        return send_from_directory(os.path.join(app.root_path, 'build'), 'index.html')
+
+    @app.route('/<path:path>')
+    def catch_all(path):
+        return send_from_directory(os.path.join(app.root_path, 'build'), 'index.html')
+
+    return app
 
 app = create_app()
 

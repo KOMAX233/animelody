b4ce1ba1962a6c845c9e7c109c4c2ebfc031dbcd
KOMAX233 <fajasmine@outlook.com>
2025-01-14 11:14:42 -0500

    added drag handler, progress bar ref, and disabled text selection and default action during dragging

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 interface ProgressBarProps {
   progress: number;
@@ -6,15 +6,51 @@ interface ProgressBarProps {
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ progress, onSeek }) => {
-  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const progressBarRef = useRef<HTMLDivElement | null>(null);
+
+  const calculatePercentage = (event: MouseEvent | React.MouseEvent) => {
+    if (!progressBarRef.current) {
+      return 0;
+    }
     const rect = (event.target as HTMLElement).getBoundingClientRect();
     const clickX = event.clientX - rect.left;
     const percentage = (clickX / rect.width) * 100;
+    return percentage;
+  }
+
+  const handleMouseDown = (event : React.MouseEvent) => {
+    setIsDragging(true);
+    event.preventDefault();
+    document.body.style.userSelect = "none";
+    const percentage = calculatePercentage(event);
+    onSeek(percentage);
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+  }
+  const handleMouseMove = (event : MouseEvent) => {
+    if (!isDragging) {
+      return;
+    }
+    const percentage = calculatePercentage(event);
+    return percentage;
+  }
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    document.body.style.userSelect = "";
+    window.removeEventListener("mousemove", handleMouseMove);
+    window.removeEventListener("mouseup", handleMouseUp);
+  }
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const percentage = calculatePercentage(event);
     onSeek(percentage);
   };
 
   return (
     <div
+      ref={progressBarRef}
       style={{
         position: "relative",
         width: "100%",
@@ -22,7 +58,9 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ progress, onSeek }) => {
         backgroundColor: "#ddd",
         borderRadius: "5px",
         cursor: "pointer",
+        userSelect: "none",
       }}
+      onMouseDown={handleMouseDown}
       onClick={handleClick}
     >
       <div
